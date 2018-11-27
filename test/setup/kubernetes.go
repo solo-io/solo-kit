c832f56dc3a702295562e540a3fae9aaf6219d50
Scott Weiss <sdw35@cornell.edu>
2018-11-26 22:03:06 -0500

    test methods for curl through testrunner
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- test/setup/kubernetes.go
+++ test/setup/kubernetes.go
@@ -3,6 +3,7 @@ package setup
 import (
 	"bytes"
 	"fmt"
+	"github.com/onsi/gomega"
 	"os"
 	"os/exec"
 	"strings"
@@ -194,4 +195,82 @@ func WaitNamespaceStatus(namespace, status string, finished func(output string) 
 			}
 		}
 	}
+}
+
+type CurlOpts struct {
+	Protocol      string
+	Path          string
+	Method        string
+	Host          string
+	Service       string
+	CaFile        string
+	Body          string
+	Headers       map[string]string
+	Port          int
+	ReturnHeaders bool
+}
+
+func CurlEventuallyShouldRespond(opts CurlOpts, substr string, timeout ...time.Duration) {
+	t := time.Second * 20
+	if len(timeout) > 0 {
+		t = timeout[0]
+	}
+	// for some useful-ish output
+	tick := time.Tick(t / 8)
+	gomega.Eventually(func() string {
+		res, err := Curl(opts)
+		if err != nil {
+			res = err.Error()
+		}
+		select {
+		default:
+			break
+		case <-tick:
+			log.GreyPrintf("running: %v\nwant %v\nhave: %s", opts, substr, res)
+		}
+		if strings.Contains(res, substr) {
+			log.GreyPrintf("success: %v", res)
+		}
+		return res
+	}, t, "5s").Should(gomega.ContainSubstring(substr))
+}
+
+func Curl(opts CurlOpts) (string, error) {
+	args := []string{"curl", "-v", "--connect-timeout", "10", "--max-time", "10"}
+
+	if opts.ReturnHeaders {
+		args = append(args, "-I")
+	}
+
+	if opts.Method != "GET" && opts.Method != "" {
+		args = append(args, "-X"+opts.Method)
+	}
+	if opts.Host != "" {
+		args = append(args, "-H", "Host: "+opts.Host)
+	}
+	if opts.CaFile != "" {
+		args = append(args, "--cacert", opts.CaFile)
+	}
+	if opts.Body != "" {
+		args = append(args, "-H", "Content-Type: application/json")
+		args = append(args, "-d", opts.Body)
+	}
+	for h, v := range opts.Headers {
+		args = append(args, "-H", fmt.Sprintf("%v: %v", h, v))
+	}
+	port := opts.Port
+	if port == 0 {
+		port = 8080
+	}
+	protocol := opts.Protocol
+	if protocol == "" {
+		protocol = "http"
+	}
+	service := opts.Service
+	if service == "" {
+		service = "test-ingress"
+	}
+	args = append(args, fmt.Sprintf("%v://%s:%v%s", protocol, service, port, opts.Path))
+	log.Debugf("running: curl %v", strings.Join(args, " "))
+	return TestRunner(args...)
 }
