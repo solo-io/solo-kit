596f6d94c295fc6c4a7a16dbc648837188c99074
Scott Weiss <sdw35@cornell.edu>
2018-11-27 14:36:15 -0500

    add stringutils
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ pkg/utils/stringutils/strings.go
@@ -0,0 +1,37 @@
+package stringutils
+
+import (
+	"reflect"
+	"sort"
+)
+
+func ContainsString(s string, slice []string) bool {
+	for _, s2 := range slice {
+		if s2 == s {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsMap(maps []map[string]string, item map[string]string) bool {
+	for _, m := range maps {
+		if reflect.DeepEqual(m, item) {
+			return true
+		}
+	}
+	return false
+}
+
+func KeysAndValues(m map[string]string) ([]string, []string) {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	var values [] string
+	for _, k := range keys {
+		values = append(values, m[k])
+	}
+	return keys, values
+}
