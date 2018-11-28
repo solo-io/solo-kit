d82976d02448b6f900cb2e292cb13d00caf1f405
EItanya <eitan.yarmush@solo.io>
2018-11-28 13:37:26 -0500

    Removed unused code

--- pkg/utils/cliutils/printer.go
+++ pkg/utils/cliutils/printer.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
 	"io"
-	"reflect"
 	"strings"
 	"text/template"
 
@@ -31,19 +30,6 @@ func Print(output, template string, m proto.Message, tblPrn Printer, w io.Writer
 	}
 }
 
-// PrintList - prints the given list of values to io.Writer using the specified output format
-func PrintList(output, template string, list interface{}, tblPrn Printer, w io.Writer) error {
-	switch strings.ToLower(output) {
-	case "yaml":
-		return PrintYAMLList(list, w)
-	case "json":
-		return PrintJSONList(list, w)
-	case "template":
-		return PrintTemplate(list, template, w)
-	default:
-		return tblPrn(list, w)
-	}
-}
 
 // PrintJSON - prints the given proto.Message to io.Writer in JSON
 func PrintJSON(m proto.Message, w io.Writer) error {
@@ -67,51 +53,6 @@ func PrintYAML(m proto.Message, w io.Writer) error {
 	}
 	_, err = fmt.Fprintln(w, string(b))
 	return err
-}
-
-// PrintJSONList - prints the given list to io.Writer in JSON
-func PrintJSONList(data interface{}, w io.Writer) error {
-	list := reflect.ValueOf(data)
-	_, err := fmt.Fprintln(w, "[")
-	if err != nil {
-		return errors.Wrap(err, "unable to print JSON list")
-	}
-	for i := 0; i < list.Len(); i++ {
-		v, ok := list.Index(i).Interface().(proto.Message)
-		if !ok {
-			return errors.New("unable to convert to proto message")
-		}
-		if i != 0 {
-			_, err = fmt.Fprintln(w, ",")
-			if err != nil {
-				return errors.Wrap(err, "unable to print JSON list")
-			}
-		}
-		err = PrintJSON(v, w)
-		if err != nil {
-			return err
-		}
-	}
-	_, err = fmt.Fprintln(w, "]")
-	return err
-}
-
-// PrintYAMLList - prints the given list to io.Writer in YAML
-func PrintYAMLList(data interface{}, w io.Writer) error {
-	list := reflect.ValueOf(data)
-	for i := 0; i < list.Len(); i++ {
-		v, ok := list.Index(i).Interface().(proto.Message)
-		if !ok {
-			return errors.New("unable to convert to proto message")
-		}
-		if _, err := fmt.Fprintln(w, "---"); err != nil {
-			return errors.Wrap(err, "unable to print YAML list")
-		}
-		if err := PrintYAML(v, w); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 // PrintTemplate prints the give value using the provided Go template to io.Writer
