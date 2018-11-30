0714377597a8bb85db20153af15c72c32b0633ed
Eitan Yarmush <eitan.yarmush@solo.io>
2018-11-30 17:26:22 -0500

    Cli utils (#16)
    * started adding cli utils to solo-kit
    
    * Removed unecessary code
    
    * Removed unused code
    
    * added support for printing lists
    
    * yml support

--- pkg/utils/cliutils/printer.go
+++ pkg/utils/cliutils/printer.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
 	"io"
+	"reflect"
 	"strings"
 	"text/template"
-
 	"github.com/ghodss/yaml"
 	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
@@ -21,6 +21,8 @@ func Print(output, template string, m proto.Message, tblPrn Printer, w io.Writer
 	switch strings.ToLower(output) {
 	case "yaml":
 		return PrintYAML(m, w)
+	case "yml":
+		return PrintYAML(m, w)
 	case "json":
 		return PrintJSON(m, w)
 	case "template":
@@ -29,8 +31,6 @@ func Print(output, template string, m proto.Message, tblPrn Printer, w io.Writer
 		return tblPrn(m, w)
 	}
 }
-
-
 // PrintJSON - prints the given proto.Message to io.Writer in JSON
 func PrintJSON(m proto.Message, w io.Writer) error {
 	b, err := protoutils.MarshalBytes(m)
@@ -62,4 +62,65 @@ func PrintTemplate(data interface{}, tmpl string, w io.Writer) error {
 		return errors.Wrap(err, "unable to parse template")
 	}
 	return t.Execute(w, data)
+}
+
+// PrintList - prints the given list of values to io.Writer using the specified output format
+func PrintList(output, template string, list interface{}, tblPrn Printer, w io.Writer) error {
+	switch strings.ToLower(output) {
+	case "yaml":
+		return PrintYAMLList(list, w)
+	case "yml":
+		return PrintYAMLList(list, w)
+	case "json":
+		return PrintJSONList(list, w)
+	case "template":
+		return PrintTemplate(list, template, w)
+	default:
+		return tblPrn(list, w)
+	}
+}
+
+// PrintJSONList - prints the given list to io.Writer in JSON
+func PrintJSONList(data interface{}, w io.Writer) error {
+	list := reflect.ValueOf(data)
+	_, err := fmt.Fprintln(w, "[")
+	if err != nil {
+		return errors.Wrap(err, "unable to print JSON list")
+	}
+	for i := 0; i < list.Len(); i++ {
+		v, ok := list.Index(i).Interface().(proto.Message)
+		if !ok {
+			return errors.New("unable to convert to proto message")
+		}
+		if i != 0 {
+			_, err = fmt.Fprintln(w, ",")
+			if err != nil {
+				return errors.Wrap(err, "unable to print JSON list")
+			}
+		}
+		err = PrintJSON(v, w)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprintln(w, "]")
+	return err
+}
+
+// PrintYAMLList - prints the given list to io.Writer in YAML
+func PrintYAMLList(data interface{}, w io.Writer) error {
+	list := reflect.ValueOf(data)
+	for i := 0; i < list.Len(); i++ {
+		v, ok := list.Index(i).Interface().(proto.Message)
+		if !ok {
+			return errors.New("unable to convert to proto message")
+		}
+		if _, err := fmt.Fprintln(w, "---"); err != nil {
+			return errors.Wrap(err, "unable to print YAML list")
+		}
+		if err := PrintYAML(v, w); err != nil {
+			return err
+		}
+	}
+	return nil
 }
