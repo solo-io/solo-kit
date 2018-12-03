9eb6b351e4083fc41f412c1931954f1ea99cb74f
Scott Weiss <sdw35@cornell.edu>
2018-12-03 13:56:16 -0500

    add tracking js to readme templates
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- pkg/code-generator/docgen/templates/resource_group_template.go
+++ pkg/code-generator/docgen/templates/resource_group_template.go
@@ -8,4 +8,8 @@ import (
 
 var ResourceGroupTemplate = template.Must(template.New("rg").Funcs(templates.Funcs).Parse(`
 {{ . }}
+
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5130874.js"></script>
+<!-- End of HubSpot Embed Code -->
 `))
