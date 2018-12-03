8fab12daf1b02b018c1cfdee7101d7fbcb218bd5
Scott Weiss <sdw35@cornell.edu>
2018-12-03 15:29:36 -0500

    move docs dir
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- test/mocks/generate.sh
+++ test/mocks/generate.sh
@@ -7,14 +7,16 @@ SOLO_KIT=${ROOT}/github.com/solo-io/solo-kit
 TEST_IN=${SOLO_KIT}/test/mocks
 OUT=${SOLO_KIT}/test/mocks/v1
 
-RELATIVE_ROOT=../../..
+DOCS_OUT=../../../
+PROJECT_IN=${PWD}/project.json
+DOCS_OUT=./doc/docs
 
 IMPORTS="-I=${TEST_IN} \
     -I=${SOLO_KIT}/api/external \
     -I=${ROOT}"
 
 GOGO_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:${GOPATH}/src/"
-SOLO_KIT_FLAG="--plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} --solo-kit_opt=${PWD}/project.json,${RELATIVE_ROOT}/doc/docs"
+SOLO_KIT_FLAG="--plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} --solo-kit_opt=${PROJECT_IN},${DOCS_OUT}"
 INPUT_PROTOS="${TEST_IN}/*.proto"
 
 mkdir -p ${OUT}
