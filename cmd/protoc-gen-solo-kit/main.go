34af26e4fbc45e95102bf1ae61045571cdd9b78d
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-12-06 11:12:17 -0500

    move main to allow other projects generate the plugin, and thus vendor it

--- cmd/protoc-gen-solo-kit/main.go
+++ cmd/protoc-gen-solo-kit/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/pseudomuto/protokit"
 	"github.com/solo-io/solo-kit/pkg/code-generator"
 )
 
 func main() {
-	outputDescriptors := os.Getenv("OUTPUT_DESCRIPTORS") == "1"
-	mergeOutputDescriptors := os.Getenv("OUTPUT_MERGED_DESCRIPTORS_FILE")
-	plugin := &code_generator.Plugin{OutputDescriptors: outputDescriptors, MergeDescriptors: mergeOutputDescriptors}
-	// use this to debug without running protoc
-	if descriptorsFile := os.Getenv("USE_DESCRIPTORS"); descriptorsFile != "" {
-		// descriptorsFile e.g.: "projects/supergloo/api/v1/project.json.descriptors"
-		f, err := os.Open(descriptorsFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := protokit.RunPluginWithIO(plugin, f, os.Stdout); err != nil {
-			log.Fatal(err)
-		}
-	}
-	if err := protokit.RunPlugin(plugin); err != nil {
-		log.Fatal(err)
-	}
+	code_generator.Main()
 }
