b4f3ee5c568a944e356f9f3fedda02624c508916
Scott Weiss <sdw35@cornell.edu>
2018-12-31 10:01:27 -0500

    Regenerate solo kit protos (#46)
    * add solo-kit proto options
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>
    
    * use gogo proto descriptors
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ api/v1/generate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+ROOT=${GOPATH}/src
+SOLO_KIT=${ROOT}/github.com/solo-io/solo-kit
+IN=${SOLO_KIT}/api/v1/
+EXTERNAL=${SOLO_KIT}/api/external/
+OUT=${SOLO_KIT}/pkg/api/external/istio/encryption/v1/
+
+IMPORTS="\
+    -I=${IN} \
+    -I=${EXTERNAL} \
+    -I=${ROOT}
+    "
+
+GOGO_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:${GOPATH}/src/"
+INPUT_PROTOS="${IN}/*.proto"
+
+mkdir -p ${OUT}
+protoc ${IMPORTS} \
+    ${GOGO_FLAG} \
+    ${INPUT_PROTOS}
