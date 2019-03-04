6dbbf377211f93ed8dbc6689bfc6cf5aa4a3f821
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-03-04 10:15:29 -0500

    solve in memory client data race (#92)
    * solve in memory client data race
    
    * fix test

--- pkg/api/v1/clients/memory/resource_client_test.go
+++ pkg/api/v1/clients/memory/resource_client_test.go
@@ -4,7 +4,10 @@ import (
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	. "github.com/solo-io/solo-kit/pkg/api/v1/clients/memory"
+	core "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	v1 "github.com/solo-io/solo-kit/test/mocks/v1"
 	"github.com/solo-io/solo-kit/test/tests/generic"
 )
@@ -20,5 +23,31 @@ var _ = Describe("Base", func() {
 	})
 	It("CRUDs resources", func() {
 		generic.TestCrudClient("", client, time.Minute)
+	})
+	It("should not return pointer to internal object", func() {
+		obj := &v1.MockResource{
+			Metadata: core.Metadata{
+				Namespace: "ns",
+				Name:      "n",
+			},
+			Data: "test",
+		}
+		client.Write(obj, clients.WriteOpts{})
+		ret, err := client.Read("ns", "n", clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(ret).NotTo(BeIdenticalTo(obj))
+
+		ret2, err := client.Read("ns", "n", clients.ReadOpts{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(ret).NotTo(BeIdenticalTo(ret2))
+
+		listret, err := client.List("ns", clients.ListOpts{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(listret[0]).NotTo(BeIdenticalTo(obj))
+
+		listret2, err := client.List("ns", clients.ListOpts{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(listret[0]).NotTo(BeIdenticalTo(listret2[0]))
+
 	})
 })
