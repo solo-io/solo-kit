44ffd5f223fbdd7106e8fc9de9ee67870ec3131a
Rick Ducott <rick.ducott@solo.io>
2019-04-26 11:20:29 -0400

    Add utility for converting sk crd -> kube crd

---
+++ pkg/utils/kubeutils/kube_crd.go
@@ -0,0 +1,35 @@
+package kubeutils
+
+import (
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube/crd"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// The standard CRD we use for helm charts, based on the solo kit CRD
+func GetCustomResourceDefinition(skCrd crd.Crd, labels map[string]string) *v1beta1.CustomResourceDefinition {
+	return &v1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: skCrd.FullName(),
+			Annotations: map[string]string{
+				"helm.sh/hook": "crd-install",
+			},
+			Labels: labels,
+		},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: skCrd.Group,
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Kind:       skCrd.KindName,
+				ListKind:   skCrd.KindName + "List",
+				Plural:     skCrd.Plural,
+				ShortNames: []string{skCrd.ShortName},
+			},
+			Scope:   "Namespaced",
+			Version: "v1",
+		},
+	}
+}
