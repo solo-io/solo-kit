a0e7a6bced3a24cd33c0453babf159863947b3e5
EItanya <eitan.yarmush@solo.io>
2019-04-30 14:48:42 -0400

    last mockgen change I hope

--- pkg/api/v1/resources/common/kubernetes/mocks/kube_namespace_reconciler.sk.go
+++ pkg/api/v1/resources/common/kubernetes/mocks/kube_namespace_reconciler.sk.go
@@ -5,10 +5,11 @@
 package mocks
 
 import (
+	reflect "reflect"
+
 	gomock "github.com/golang/mock/gomock"
 	clients "github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	kubernetes "github.com/solo-io/solo-kit/pkg/api/v1/resources/common/kubernetes"
-	reflect "reflect"
 )
 
 // MockKubeNamespaceReconciler is a mock of KubeNamespaceReconciler interface
