c2742702f50f955dc652e8b1ec8650fa9364b5b0
scott weiss <sdw35@cornell.edu>
2019-05-03 11:42:23 -0400

    nits
    Signed-off-by: scott weiss <sdw35@cornell.edu>

--- test/util/locking_suite.go
+++ test/util/locking_suite.go
@@ -1,28 +1,23 @@
 package util
 
 import (
-	"github.com/solo-io/go-utils/kubeutils"
-	"github.com/solo-io/go-utils/testutils/clusterlock"
-	"k8s.io/client-go/kubernetes"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/go-utils/testutils/clusterlock"
+	"github.com/solo-io/go-utils/testutils/kube"
 )
 
 // call this function with a var _ = in the test suite file
 // tests will panic if there's a previously defined BeforeSuite or AfterSuite
 func LockingSuite() bool {
 	var (
-		lock       *clusterlock.TestClusterLocker
-		kubeClient kubernetes.Interface
+		lock *clusterlock.TestClusterLocker
 	)
 
 	var _ = BeforeEach(func() {
-		cfg, err := kubeutils.GetConfig("", "")
+		var err error
+		lock, err = clusterlock.NewTestClusterLocker(kube.MustKubeClient(), clusterlock.Options{})
 		Expect(err).NotTo(HaveOccurred())
-		kubeClient, err = kubernetes.NewForConfig(cfg)
-		Expect(err).NotTo(HaveOccurred())
-		lock, err = clusterlock.NewTestClusterLocker(kubeClient, clusterlock.Options{})
 		Expect(lock.AcquireLock()).NotTo(HaveOccurred())
 	})
 
