2362dcfb9214a47de47f4dc13bdca7855fb95fc7
EItanya <eitan.yarmush@solo.io>
2019-06-27 11:25:03 -0400

    scott...

--- pkg/code-generator/cmd/main.go
+++ pkg/code-generator/cmd/main.go
@@ -60,9 +60,25 @@ type GenerateOptions struct {
 type DescriptorWithPath struct {
 	*descriptor.FileDescriptorProto
 
-	// the absolute path from which the proto file was read
-	// we use this to correlate messages with their respective solo-kit.json files
+	// the absolute paths from which the proto file was read
+	// we use these to correlate messages with their respective solo-kit.json files
+	ProtoFilePaths []string
+	// Computed from all possible import paths
 	ProtoFilePath string
+}
+
+func (d *DescriptorWithPath) SelectProtoFilePath() {
+	switch len(d.ProtoFilePaths) {
+	case 1:
+		d.ProtoFilePath = d.ProtoFilePaths[0]
+	default:
+		for _, path := range d.ProtoFilePaths {
+			if path != "" && strings.Contains(path, d.GetName()) {
+				d.ProtoFilePath = path
+				break
+			}
+		}
+	}
 }
 
 func Generate(opts GenerateOptions) error {
@@ -113,9 +129,9 @@ func Generate(opts GenerateOptions) error {
 		return err
 	}
 
-	// for _, desc := range descriptors {
-	// 	desc.SelectProtoFilePath()
-	// }
+	for _, desc := range descriptors {
+		desc.SelectProtoFilePath()
+	}
 
 	log.Printf("collected descriptors: %v", func() []string {
 		var names []string
@@ -324,10 +340,7 @@ func addDescriptorsForFile(addDescriptor func(f DescriptorWithPath), root, proto
 	}
 
 	for _, f := range desc.File {
-		if strings.HasSuffix(protoFile, f.GetName()) {
-			addDescriptor(DescriptorWithPath{FileDescriptorProto: f, ProtoFilePath: protoFile})
-		}
-		addDescriptor(DescriptorWithPath{FileDescriptorProto: f, ProtoFilePath: ""})
+		addDescriptor(DescriptorWithPath{FileDescriptorProto: f, ProtoFilePaths: []string{protoFile}})
 	}
 
 	return nil
@@ -343,11 +356,13 @@ func collectDescriptorsFromRoot(root string, customImports, customGogoArgs, skip
 		// with different import paths
 		for _, existing := range descriptors {
 			if existing.GetName() == f.GetName() {
+				existing.ProtoFilePaths = append(existing.ProtoFilePaths, f.ProtoFilePaths...)
 				return
 			}
 			existingCopy := proto.Clone(existing.FileDescriptorProto).(*descriptor.FileDescriptorProto)
 			existingCopy.Name = f.Name
 			if proto.Equal(existingCopy, f.FileDescriptorProto) {
+				existing.ProtoFilePaths = append(existing.ProtoFilePaths, f.ProtoFilePaths...)
 				return
 			}
 		}
