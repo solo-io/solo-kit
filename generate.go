bf617114a99a764ddf8d9749a07ba29152f45a0b
Scott Weiss <sdw35@cornell.edu>
2019-09-10 12:04:24 -0400

    remove generated docs see #268 for details

--- generate.go
+++ generate.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/solo-io/go-utils/log"
 	"github.com/solo-io/solo-kit/pkg/code-generator/cmd"
-	"github.com/solo-io/solo-kit/pkg/code-generator/docgen/options"
 )
 
 //go:generate go run generate.go
@@ -15,9 +14,6 @@ func main() {
 		RelativeRoot:  ".",
 		CompileProtos: true,
 		SkipGenMocks:  true,
-		GenDocs: &cmd.DocsOptions{
-			Output: options.Hugo,
-		},
 	}); err != nil {
 		log.Fatalf("generate failed!: %v", err)
 	}
