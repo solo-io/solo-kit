bbd69f1bf82b0bbad4113ba3bff3ea5588b41ae8
EItanya <eitan.yarmush@solo.io>
2019-09-16 12:03:12 -0400

    added owner ref, and fixed generation

--- pkg/utils/kubeutils/metadata.go
+++ pkg/utils/kubeutils/metadata.go
@@ -1,9 +1,11 @@
 package kubeutils
 
 import (
+	"github.com/gogo/protobuf/types"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubetypes "k8s.io/apimachinery/pkg/types"
 )
 
 func FromKubeMeta(meta metav1.ObjectMeta) core.Metadata {
@@ -14,6 +16,7 @@ func FromKubeMeta(meta metav1.ObjectMeta) core.Metadata {
 		Labels:          copyMap(meta.Labels),
 		Annotations:     copyMap(meta.Annotations),
 		Generation:      meta.Generation,
+		OwnerReferences: copyKubernetesOwnerReferences(meta.OwnerReferences),
 	}
 }
 
@@ -25,7 +28,53 @@ func ToKubeMeta(meta core.Metadata) metav1.ObjectMeta {
 		Labels:          copyMap(meta.Labels),
 		Annotations:     copyMap(meta.Annotations),
 		Generation:      meta.Generation,
+		OwnerReferences: copySoloKitOwnerReferences(meta.GetOwnerReferences()),
 	}
+}
+
+func copyKubernetesOwnerReferences(references []metav1.OwnerReference) []*core.Metadata_OwnerReference {
+	result := make([]*core.Metadata_OwnerReference, len(references))
+	for _, ref := range references {
+		skRef := &core.Metadata_OwnerReference{
+			ApiVersion: ref.APIVersion,
+			Kind:       ref.Kind,
+			Name:       ref.APIVersion,
+			Uid:        string(ref.UID),
+		}
+		if ref.Controller != nil {
+			skRef.Controller = &types.BoolValue{
+				Value: *ref.Controller,
+			}
+		}
+		if ref.BlockOwnerDeletion != nil {
+			skRef.BlockOwnerDeletion = &types.BoolValue{
+				Value: *ref.BlockOwnerDeletion,
+			}
+		}
+		result = append(result, skRef)
+	}
+	return result
+}
+
+func copySoloKitOwnerReferences(skReferences []*core.Metadata_OwnerReference) []metav1.OwnerReference {
+	result := make([]metav1.OwnerReference, len(skReferences))
+	for _, skRef := range skReferences {
+		ref := &metav1.OwnerReference{
+			APIVersion: skRef.GetApiVersion(),
+			Kind:       skRef.GetKind(),
+			Name:       skRef.GetName(),
+			UID:        kubetypes.UID(skRef.GetUid()),
+		}
+		if skRef.GetBlockOwnerDeletion() != nil {
+			boolValue := skRef.GetBlockOwnerDeletion().GetValue()
+			ref.BlockOwnerDeletion = &boolValue
+		}
+		if skRef.GetController() != nil {
+			boolValue := skRef.GetController().GetValue()
+			ref.Controller = &boolValue
+		}
+	}
+	return result
 }
 
 func copyMap(m map[string]string) map[string]string {
