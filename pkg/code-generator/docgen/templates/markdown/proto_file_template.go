60015bef6312a5178344b53743183c846d3cc534
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-09-18 12:52:44 -0400

    don't add a newline (#278)
    * don't add a newline
    * changelog and tweaks

--- pkg/code-generator/docgen/templates/markdown/proto_file_template.go
+++ pkg/code-generator/docgen/templates/markdown/proto_file_template.go
@@ -64,10 +64,9 @@ func ProtoFileTemplate(project *model.Project, docsOptions *options.DocsOptions)
 | Field | Type | Description | Default |
 | ----- | ---- | ----------- |----------- | 
 {{range .Fields -}}
-{{ $description := remove_magic_comments (nobr .Comments.Leading) }}
-{{ $oneofmsg := getOneofMessage . }}
-
-| {{backtick}}{{ lower_camel (printfptr "%v" .Name) }}{{backtick}} | {{linkForField (getFileForMessage $Message) . }} | {{ if $description }} {{trimSuffix "." $description}}.{{ end }} {{ if $oneofmsg }} {{$oneofmsg}}{{ end }} | {{if .DefaultValue}} Default: {{.DefaultValue}}{{end}} |
+{{- $description := remove_magic_comments (nobr .Comments.Leading) -}}
+{{- $oneofmsg := getOneofMessage . -}}
+| {{backtick}}{{ lower_camel (printfptr "%v" .Name) }}{{backtick}} | {{linkForField (getFileForMessage $Message) . }} | {{ if $description }}{{trimSuffix "." $description}}.{{ end }}{{ if $oneofmsg }} {{$oneofmsg}}{{ end }} | {{if .DefaultValue}} Default: {{.DefaultValue}}{{end}} |
 {{end}}
 
 ` + "`" + ` }}
