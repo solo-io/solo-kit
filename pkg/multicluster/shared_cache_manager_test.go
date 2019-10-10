18d27e2b28b31bf01086b541e6e06183a823a65c
Joe Kelley <jsphklly@gmail.com>
2019-10-10 13:39:33 -0400

    test shared cache manager

--- pkg/multicluster/shared_cache_manager_test.go
+++ pkg/multicluster/shared_cache_manager_test.go
@@ -6,22 +6,41 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/solo-io/solo-kit/pkg/multicluster"
+	"k8s.io/client-go/rest"
 )
 
-var _ = Describe("Shared Cache Manager", func() {
+var _ = FDescribe("Shared Cache Manager", func() {
 	var manager multicluster.KubeSharedCacheManager
 
 	BeforeEach(func() {
 		manager = multicluster.NewKubeSharedCacheManager(context.Background())
 	})
 
-	It("works", func() {
+	It("assigns a unique cache to each cluster", func() {
 		cluster1, cluster2 := "one", "two"
-
 		cache1 := manager.GetCache(cluster1)
 		Expect(cache1).NotTo(BeNil())
 		cache2 := manager.GetCache(cluster2)
 		Expect(cache2).NotTo(BeNil())
-		Expect(cache1 == cache2).To(BeFalse(), "each has a unique cache")
+		Expect(cache1).NotTo(BeIdenticalTo(cache2))
+	})
+
+	It("assigns one and only one cache to a given cluster", func() {
+		cluster := "one"
+		cache := manager.GetCache(cluster)
+		Expect(cache).NotTo(BeNil())
+		sameCache := manager.GetCache(cluster)
+		Expect(sameCache).NotTo(BeNil())
+		Expect(cache).To(BeIdenticalTo(sameCache))
+	})
+
+	It("creates a new cache on a subsequent call to GetCache after a cluster is removed", func() {
+		cluster := "one"
+		firstCache := manager.GetCache(cluster)
+		Expect(firstCache).NotTo(BeNil())
+		manager.ClusterRemoved(cluster, &rest.Config{})
+		secondCache := manager.GetCache(cluster)
+		Expect(secondCache).NotTo(BeNil())
+		Expect(firstCache).NotTo(BeIdenticalTo(secondCache))
 	})
 })
