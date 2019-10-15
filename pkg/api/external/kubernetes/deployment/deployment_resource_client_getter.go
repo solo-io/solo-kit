697978e8e294757c13b9828c5911e68015ac10f8
Joe Kelley <jsphklly@gmail.com>
2019-10-15 14:51:22 -0400

    deployments

--- pkg/api/external/kubernetes/deployment/deployment_resource_client_getter.go
+++ pkg/api/external/kubernetes/deployment/deployment_resource_client_getter.go
@@ -1,23 +1,21 @@
 package deployment
 
 import (
-	"context"
-
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
-	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube/cache"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/multicluster"
+	"github.com/solo-io/solo-kit/pkg/multicluster/clustercache"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 type deploymentResourceClientGetter struct {
-	ctx context.Context
+	cacheGetter clustercache.KubeDeploymentCacheGetter
 }
 
 var _ multicluster.ClientGetter = &deploymentResourceClientGetter{}
 
-func NewDeploymentResourceClientGetter(ctx context.Context) *deploymentResourceClientGetter {
-	return &deploymentResourceClientGetter{ctx: ctx}
+func NewDeploymentResourceClientGetter(cacheGetter clustercache.KubeDeploymentCacheGetter) *deploymentResourceClientGetter {
+	return &deploymentResourceClientGetter{cacheGetter: cacheGetter}
 }
 
 func (g *deploymentResourceClientGetter) GetClient(cluster string, restConfig *rest.Config) (clients.ResourceClient, error) {
@@ -25,9 +23,5 @@ func (g *deploymentResourceClientGetter) GetClient(cluster string, restConfig *r
 	if err != nil {
 		return nil, err
 	}
-	deploymentCache, err := cache.NewKubeDeploymentCache(g.ctx, kube)
-	if err != nil {
-		return nil, err
-	}
-	return newResourceClient(kube, deploymentCache), nil
+	return newResourceClient(kube, g.cacheGetter.GetCache(cluster, restConfig)), nil
 }
