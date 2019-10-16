92ffea10ef1d6579e8e73513d9d339fef789973d
Joe Kelley <jsphklly@gmail.com>
2019-10-16 17:49:32 -0400

    test

--- pkg/multicluster/clustercache/cache_manager_test.go
+++ pkg/multicluster/clustercache/cache_manager_test.go
@@ -17,36 +17,40 @@ var _ = Describe("Cache Manager", func() {
 		err        error
 	)
 
-	BeforeEach(func() {
-		manager, err = NewCacheManager(context.Background(), kube.FromConfig)
-		Expect(err).NotTo(HaveOccurred())
-	})
+	Context("cache manager for kube shared cache", func() {
+		BeforeEach(func() {
+			manager, err = NewCacheManager(context.Background(), kube.FromConfig)
+			Expect(err).NotTo(HaveOccurred())
+		})
 
-	It("assigns a unique cache to each cluster", func() {
-		cluster1, cluster2 := "one", "two"
-		cache1 := manager.GetCache(cluster1, restConfig)
-		Expect(cache1).NotTo(BeNil())
-		cache2 := manager.GetCache(cluster2, restConfig)
-		Expect(cache2).NotTo(BeNil())
-		Expect(cache1).NotTo(BeIdenticalTo(cache2))
-	})
+		It("assigns a unique cache to each cluster", func() {
+			cluster1, cluster2 := "one", "two"
+			cache1 := manager.GetCache(cluster1, restConfig)
+			Expect(cache1).NotTo(BeNil())
+			cache2 := manager.GetCache(cluster2, restConfig)
+			Expect(cache2).NotTo(BeNil())
+			Expect(cache1).NotTo(BeIdenticalTo(cache2))
+			_, ok := cache1.(kube.SharedCache)
+			Expect(ok).To(BeTrue(), "consumers can extract the type of cache they're getting")
+		})
 
-	It("assigns one and only one cache to a given cluster", func() {
-		cluster := "one"
-		cache := manager.GetCache(cluster, restConfig)
-		Expect(cache).NotTo(BeNil())
-		sameCache := manager.GetCache(cluster, restConfig)
-		Expect(sameCache).NotTo(BeNil())
-		Expect(cache).To(BeIdenticalTo(sameCache))
-	})
+		It("assigns one and only one cache to a given cluster", func() {
+			cluster := "one"
+			cache := manager.GetCache(cluster, restConfig)
+			Expect(cache).NotTo(BeNil())
+			sameCache := manager.GetCache(cluster, restConfig)
+			Expect(sameCache).NotTo(BeNil())
+			Expect(cache).To(BeIdenticalTo(sameCache))
+		})
 
-	It("creates a new cache on a subsequent call to GetCache after a cluster is removed", func() {
-		cluster := "one"
-		firstCache := manager.GetCache(cluster, restConfig)
-		Expect(firstCache).NotTo(BeNil())
-		manager.ClusterRemoved(cluster, restConfig)
-		secondCache := manager.GetCache(cluster, restConfig)
-		Expect(secondCache).NotTo(BeNil())
-		Expect(firstCache).NotTo(BeIdenticalTo(secondCache))
+		It("creates a new cache on a subsequent call to GetCache after a cluster is removed", func() {
+			cluster := "one"
+			firstCache := manager.GetCache(cluster, restConfig)
+			Expect(firstCache).NotTo(BeNil())
+			manager.ClusterRemoved(cluster, restConfig)
+			secondCache := manager.GetCache(cluster, restConfig)
+			Expect(secondCache).NotTo(BeNil())
+			Expect(firstCache).NotTo(BeIdenticalTo(secondCache))
+		})
 	})
 })
