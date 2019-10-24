9b94eda40440da26cba409b0ba701c52544b87bd
Joe Kelley <jsphklly@gmail.com>
2019-10-24 11:52:12 -0400

    gen

--- pkg/api/v1/clients/multicluster/mocks/client_manager.go
+++ pkg/api/v1/clients/multicluster/mocks/client_manager.go
@@ -5,10 +5,11 @@
 package mocks
 
 import (
+	reflect "reflect"
+
 	gomock "github.com/golang/mock/gomock"
 	clients "github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	rest "k8s.io/client-go/rest"
-	reflect "reflect"
 )
 
 // MockClusterClientHandler is a mock of ClusterClientHandler interface
