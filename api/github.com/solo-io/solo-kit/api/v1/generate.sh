c830a300ef97d0cf67c601345c69f0e6e13c750a
EItanya <eitan.yarmush@solo.io>
2019-11-27 11:15:53 -0500

    got static generation working

--- api/github.com/solo-io/solo-kit/api/v1/generate.sh
+++ api/github.com/solo-io/solo-kit/api/v1/generate.sh
@@ -2,22 +2,20 @@
 
 set -ex
 
-ROOT=../../..
-SOLO_KIT=${ROOT}/github.com/solo-io/solo-kit
+SOLO_KIT=$(dirname "${BASH_SOURCE[0]}")/../..
 IN=${SOLO_KIT}/api/v1/
 EXTERNAL=${SOLO_KIT}/api/external/
-OUT=${SOLO_KIT}/pkg/api/external/istio/encryption/v1/
+#OUT=${SOLO_KIT}/pkg/api/external/istio/encryption/v1/
 
 IMPORTS="\
     -I=${IN} \
     -I=${EXTERNAL} \
-    -I=${ROOT}
     "
 
-GOGO_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:${ROOT}"
-INPUT_PROTOS="${IN}/*.proto"
+GOGO_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:${SOLO_KIT}"
+INPUT_PROTOS="${IN}*.proto"
 
-mkdir -p ${OUT}
+#mkdir -p ${OUT}
 protoc ${IMPORTS} \
     ${GOGO_FLAG} \
     ${INPUT_PROTOS}
