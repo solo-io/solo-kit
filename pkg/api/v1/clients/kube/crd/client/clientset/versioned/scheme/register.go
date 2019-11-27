6bb54b82fcc9f0061c1ece7b3770509a0cc19f59
Marco Schmidt <marcog.schmidt@gmail.com>
2019-11-26 22:27:54 -0500

    Remove commented code

--- pkg/api/v1/clients/kube/crd/client/clientset/versioned/scheme/register.go
+++ pkg/api/v1/clients/kube/crd/client/clientset/versioned/scheme/register.go
@@ -29,27 +29,6 @@ var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
 
-//var localSchemeBuilder = runtime.SchemeBuilder{
-//	resourcesv1.AddToScheme,
-//}
-//
-//// AddToScheme adds all types of this clientset into the given scheme. This allows composition
-//// of clientsets, like in:
-////
-////   import (
-////     "k8s.io/client-go/kubernetes"
-////     clientsetscheme "k8s.io/client-go/kubernetes/scheme"
-////     aggregatorclientsetscheme "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/scheme"
-////   )
-////
-////   kclientset, _ := kubernetes.NewForConfig(c)
-////   _ = aggregatorclientsetscheme.AddToScheme(clientsetscheme.Scheme)
-////
-//// After this, RawExtensions in Kubernetes types will serialize kube-aggregator types
-//// correctly.
-//var AddToScheme = localSchemeBuilder.AddToScheme
-
 func init() {
 	v1.AddToGroupVersion(Scheme, schema.GroupVersion{Version: "v1"})
-	//utilruntime.Must(AddToScheme(Scheme))
 }
