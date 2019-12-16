bf7788abe60293a626ebeb505533197b082b0b25
EItanya <eitan.yarmush@solo.io>
2019-12-16 17:08:49 -0500

    second attempt

--- pkg/api/v1/clients/file/file_suite_test.go
+++ pkg/api/v1/clients/file/file_suite_test.go
@@ -12,3 +12,20 @@ func TestFile(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "File Suite")
 }
+
+// var (
+// 	tmpDir string
+// )
+//
+// var _ = SynchronizedBeforeSuite(func() []byte {
+// 	dbRunner = db.NewRunner()
+// 	err := dbRunner.Start()
+// 	Ω(err).ShouldNot(HaveOccurred())
+// 	return []byte(dbRunner.URL)
+// }, func(data []byte) {})
+//
+// var _ = SynchronizedAfterSuite(func() {
+// 	dbClient.Cleanup()
+// }, func() {
+// 	dbRunner.Stop()
+// })
