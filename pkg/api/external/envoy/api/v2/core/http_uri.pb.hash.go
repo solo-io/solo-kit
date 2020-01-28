05951b8a4a8d02d323ba2f5e94961c911edfd545
Marco Schmidt <marcog.schmidt@gmail.com>
2020-01-28 12:52:11 -0500

    Port fix for generated hash functions (#342)
    * Ran dep ensure
    * go get protoc-gen-ext@v0.0.7
    * Changelog
    * Codegen
    * Use existing cluster for multicluster tests

--- pkg/api/external/envoy/api/v2/core/http_uri.pb.hash.go
+++ pkg/api/external/envoy/api/v2/core/http_uri.pb.hash.go
@@ -18,6 +18,11 @@ import (
 var (
 	_ = errors.New("")
 	_ = fmt.Print
+	_ = binary.LittleEndian
+	_ = new(hash.Hash64)
+	_ = fnv.New64
+	_ = hashstructure.Hash
+	_ = new(safe_hasher.SafeHasher)
 )
 
 // Hash function
@@ -29,6 +34,9 @@ func (m *HttpUri) Hash(hasher hash.Hash64) (uint64, error) {
 		hasher = fnv.New64()
 	}
 	var err error
+	if _, err = hasher.Write([]byte("envoy.api.v2.core.github.com/solo-io/solo-kit/pkg/api/external/envoy/api/v2/core.HttpUri")); err != nil {
+		return 0, err
+	}
 
 	if _, err = hasher.Write([]byte(m.GetUri())); err != nil {
 		return 0, err
