c928fb675bf5a3c85a2863687d56029603837f73
Ashley Wang <ashley.wang@solo.io>
2020-02-18 16:53:50 -0500

    clean up external envoy API docs (#346)
    * clean up external envoy API docs
    * gofmt

--- ci/check-code-gen.sh
+++ ci/check-code-gen.sh
@@ -22,7 +22,7 @@ if [[ $? -ne 0 ]]; then
 fi
 if [[ $(git status --porcelain | wc -l) -ne 0 ]]; then
   echo "Generating code produced a non-empty diff."
-  echo "Try running 'dep ensure && make install-codegen-deps generated-code -B' then re-pushing."
+  echo "Try running 'make update-deps generated-code -B' then re-pushing."
   git status --porcelain
   git diff | cat
   exit 1;
