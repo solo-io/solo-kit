067df1e63c3cb25579a3c20a1dcc284ec66a8d8d
arianaw66 <ariana.weinstock@solo.io>
2020-05-07 16:17:13 -0400

    don't Find resources without a namespace

--- pkg/code-generator/codegen/templates/resource_template.go
+++ pkg/code-generator/codegen/templates/resource_template.go
@@ -110,8 +110,7 @@ type {{ .Name }}List []*{{ .Name }}
 // namespace is optional, if left empty, names can collide if the list contains more than one with the same name
 func (list {{ .Name }}List) Find(namespace, name string) (*{{ .Name }}, error) {
 	for _, {{ lower_camel .Name }} := range list {
-		if {{ lower_camel .Name }}.GetMetadata().Name == name {
-			if namespace == "" || {{ lower_camel .Name }}.GetMetadata().Namespace == namespace {
+		if {{ lower_camel .Name }}.GetMetadata().Name == name && {{ lower_camel .Name }}.GetMetadata().Namespace == namespace {
 				return {{ lower_camel .Name }}, nil
 			}
 		}
