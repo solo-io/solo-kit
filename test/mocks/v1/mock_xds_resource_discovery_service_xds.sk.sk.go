8018cf88f7fd1bfa1d8c35e146bf647028293cc1
EItanya <eitan.yarmush@solo.io>
2020-11-22 10:31:24 -0500

    fingers crossed

--- test/mocks/v1/mock_xds_resource_discovery_service_xds.sk.sk.go
+++ test/mocks/v1/mock_xds_resource_discovery_service_xds.sk.sk.go
@@ -73,7 +73,7 @@ func NewMockXdsResourceDiscoveryServiceServer(genericServer server.Server) MockX
 }
 
 func (s *mockXdsResourceDiscoveryServiceServer) StreamMockXdsResourceConfig(stream MockXdsResourceDiscoveryService_StreamMockXdsResourceConfigServer) error {
-	return s.Server.Stream(stream, MockXdsResourceConfigType)
+	return s.Server.StreamV2(stream, MockXdsResourceConfigType)
 }
 
 func (s *mockXdsResourceDiscoveryServiceServer) FetchMockXdsResourceConfig(ctx context.Context, req *discovery.DiscoveryRequest) (*discovery.DiscoveryResponse, error) {
@@ -81,7 +81,7 @@ func (s *mockXdsResourceDiscoveryServiceServer) FetchMockXdsResourceConfig(ctx c
 		return nil, status.Errorf(codes.Unavailable, "empty request")
 	}
 	req.TypeUrl = MockXdsResourceConfigType
-	return s.Server.Fetch(ctx, req)
+	return s.Server.FetchV2(ctx, req)
 }
 
 func (s *mockXdsResourceDiscoveryServiceServer) DeltaMockXdsResourceConfig(_ MockXdsResourceDiscoveryService_DeltaMockXdsResourceConfigServer) error {
