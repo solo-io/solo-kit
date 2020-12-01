464bf8a51fcfe6427407a6106652ead09ca590ee
EItanya <eitan.yarmush@solo.io>
2020-11-30 19:47:24 -0500

    bump protoc-gen-ext

--- pkg/api/external/envoy/type/percent.pb.equal.go
+++ pkg/api/external/envoy/type/percent.pb.equal.go
@@ -4,14 +4,25 @@
 package _type
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	equality "github.com/solo-io/protoc-gen-ext/pkg/equality"
 )
 
 // ensure the imports are used
 var (
 	_ = errors.New("")
 	_ = fmt.Print
+	_ = binary.LittleEndian
+	_ = bytes.Compare
+	_ = strings.Compare
+	_ = equality.Equalizer(nil)
+	_ = proto.Message(nil)
 )
 
 // Equal function
