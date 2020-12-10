24c0ca6d10528f7aad7240f5652b3f02740668ac
EItanya <eitan.yarmush@solo.io>
2020-12-10 11:19:53 -0500

    set non-nil status

--- pkg/api/v1/clients/kube/resource_client.go
+++ pkg/api/v1/clients/kube/resource_client.go
@@ -444,10 +444,9 @@ func (rc *ResourceClient) convertCrdToResource(resourceCrd *v1.Resource) (resour
 		// Default unmarshalling
 
 		if withStatus, ok := resource.(resources.InputResource); ok {
+			// Always initialize status to empty, before it was empty by default, as it was a non-pointer value.
+			withStatus.SetStatus(&core.Status{})
 			updateFunc := func(status *core.Status) error {
-				if status == nil {
-					return nil
-				}
 				typedStatus := core.Status{}
 				if err := protoutils.UnmarshalMapToProto(resourceCrd.Status, &typedStatus); err != nil {
 					return err
