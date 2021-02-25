4f079c6c9757d72cb2ad0cc4b1d322ed17ba42d8
Sam Heilbron <samheilbron@gmail.com>
2021-02-25 08:54:34 -0700

    update proto

--- pkg/code-generator/codegen/templates/xds_template.go
+++ pkg/code-generator/codegen/templates/xds_template.go
@@ -12,7 +12,7 @@ import (
 	"fmt"
 
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-    discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
