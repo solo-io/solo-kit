dc9453b779dbce7cad43d81ec9113926652ccc4b
npolshakova <ninapolshakova@gmail.com>
2021-03-15 14:53:06 -0400

    Set snapshot fix backport (#411)
    * Set snapshot fix (#410)
    
    * initial commit
    * changelog
    * fix issue link
    * regenerate code
    * add comment, use solo-kit resource types
    * Set snapshot fix (#410)
    
    * initial commit
    * changelog
    * fix issue link
    * regenerate code
    * add comment, use solo-kit resource types
    * fix changelog
    * fix go sum

--- pkg/api/v1/control-plane/cache/cache_suite_test.go
+++ pkg/api/v1/control-plane/cache/cache_suite_test.go
@@ -1,7 +1,22 @@
-package cache
+package cache_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
+
+	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	v32 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
+	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/solo-io/solo-kit/pkg/api/v1/control-plane/cache"
+	"github.com/solo-io/solo-kit/pkg/api/v1/control-plane/resource"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,4 +25,205 @@ import (
 func TestControlPlaneCache(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "ControlPlaneCache Suite")
+}
+
+const (
+	ListenerName = "listener_0"
+	UpstreamHost = "127.0.0.1"
+)
+
+type TestSnapshot struct {
+	// Endpoints are items in the EDS V3 response payload.
+	Endpoints cache.Resources
+
+	// Clusters are items in the CDS response payload.
+	Clusters cache.Resources
+
+	// Routes are items in the RDS response payload.
+	Routes cache.Resources
+
+	// Listeners are items in the LDS response payload.
+	Listeners cache.Resources
+}
+
+func (s TestSnapshot) Consistent() error {
+	endpoints := resource.GetResourceReferences(s.Clusters.Items)
+	if len(endpoints) != len(s.Endpoints.Items) {
+		return fmt.Errorf("mismatched endpoint reference and resource lengths: length of %v does not equal length of %v", endpoints, s.Endpoints.Items)
+	}
+	if err := cache.Superset(endpoints, s.Endpoints.Items); err != nil {
+		return err
+	}
+
+	routes := resource.GetResourceReferences(s.Listeners.Items)
+	if len(routes) != len(s.Routes.Items) {
+		return fmt.Errorf("mismatched route reference and resource lengths: length of %v does not equal length of %v", routes, s.Routes.Items)
+	}
+	return cache.Superset(routes, s.Routes.Items)
+}
+func (s TestSnapshot) GetResources(typ string) cache.Resources {
+	switch typ {
+	case resource.EndpointTypeV3:
+		return s.Endpoints
+	case resource.ClusterTypeV3:
+		return s.Clusters
+	case resource.RouteTypeV3:
+		return s.Routes
+	case resource.ListenerTypeV3:
+		return s.Listeners
+	}
+	return cache.Resources{}
+}
+func (s TestSnapshot) Clone() cache.Snapshot {
+	snapshotClone := &TestSnapshot{}
+
+	snapshotClone.Endpoints = cache.Resources{
+		Version: s.Endpoints.Version,
+		Items:   cloneItems(s.Endpoints.Items),
+	}
+
+	snapshotClone.Clusters = cache.Resources{
+		Version: s.Clusters.Version,
+		Items:   cloneItems(s.Clusters.Items),
+	}
+
+	snapshotClone.Routes = cache.Resources{
+		Version: s.Routes.Version,
+		Items:   cloneItems(s.Routes.Items),
+	}
+
+	snapshotClone.Listeners = cache.Resources{
+		Version: s.Listeners.Version,
+		Items:   cloneItems(s.Listeners.Items),
+	}
+
+	return snapshotClone
+}
+
+func cloneItems(items map[string]cache.Resource) map[string]cache.Resource {
+	clonedItems := make(map[string]cache.Resource, len(items))
+	for k, v := range items {
+		resProto := v.ResourceProto()
+		resClone := proto.Clone(resProto)
+		clonedItems[k] = resource.NewEnvoyResource(resClone)
+	}
+	return clonedItems
+}
+
+var _ cache.Snapshot = TestSnapshot{}
+
+func makeEndpoint(clusterName string) *endpoint.ClusterLoadAssignment {
+	return &endpoint.ClusterLoadAssignment{
+		ClusterName: clusterName,
+		Endpoints: []*endpoint.LocalityLbEndpoints{{
+			LbEndpoints: []*endpoint.LbEndpoint{{
+				HostIdentifier: &endpoint.LbEndpoint_Endpoint{
+					Endpoint: &endpoint.Endpoint{
+						Address: &core.Address{
+							Address: &core.Address_SocketAddress{
+								SocketAddress: &core.SocketAddress{
+									Protocol: core.SocketAddress_TCP,
+									Address:  UpstreamHost,
+									PortSpecifier: &core.SocketAddress_PortValue{
+										PortValue: uint32(8080),
+									},
+								},
+							},
+						},
+					},
+				},
+			}},
+		}},
+	}
+}
+
+func makeEDSCluster(clusterName string) *cluster.Cluster {
+	return &cluster.Cluster{
+		Name:                 clusterName,
+		ConnectTimeout:       ptypes.DurationProto(5 * time.Second),
+		ClusterDiscoveryType: &cluster.Cluster_Type{Type: cluster.Cluster_EDS},
+		LbPolicy:             cluster.Cluster_ROUND_ROBIN,
+		LoadAssignment:       makeEndpoint(clusterName),
+		DnsLookupFamily:      cluster.Cluster_V4_ONLY,
+		EdsClusterConfig: &cluster.Cluster_EdsClusterConfig{
+			EdsConfig: &v32.ConfigSource{
+				ConfigSourceSpecifier: &v32.ConfigSource_Ads{
+					Ads: &v32.AggregatedConfigSource{},
+				},
+			},
+		},
+	}
+}
+
+func makeCluster(clusterName string) *cluster.Cluster {
+	return &cluster.Cluster{
+		Name:                 clusterName,
+		ConnectTimeout:       ptypes.DurationProto(5 * time.Second),
+		ClusterDiscoveryType: &cluster.Cluster_Type{Type: cluster.Cluster_LOGICAL_DNS},
+		LbPolicy:             cluster.Cluster_ROUND_ROBIN,
+		LoadAssignment:       makeEndpoint(clusterName),
+		DnsLookupFamily:      cluster.Cluster_V4_ONLY,
+	}
+}
+
+func makeRoute(routeName string, clusterName string) *route.RouteConfiguration {
+	routeConfiguration := &route.RouteConfiguration{
+		Name: routeName,
+		VirtualHosts: []*route.VirtualHost{{
+			Name:    "local_service",
+			Domains: []string{"*"},
+		}},
+	}
+	routeConfiguration.VirtualHosts[0].Routes = []*route.Route{{
+		Match: &route.RouteMatch{
+			PathSpecifier: &route.RouteMatch_Prefix{
+				Prefix: "/",
+			},
+		},
+		Action: &route.Route_Route{
+			Route: &route.RouteAction{
+				ClusterSpecifier: &route.RouteAction_Cluster{
+					Cluster: clusterName,
+				},
+				HostRewriteSpecifier: &route.RouteAction_HostRewriteLiteral{
+					HostRewriteLiteral: UpstreamHost,
+				},
+			},
+		},
+	}}
+	return routeConfiguration
+}
+
+func makeHTTPListener(route string) *listener.Listener {
+	// HTTP filter configuration
+	manager := &hcm.HttpConnectionManager{
+		CodecType:  hcm.HttpConnectionManager_AUTO,
+		StatPrefix: "http",
+		RouteSpecifier: &hcm.HttpConnectionManager_Rds{
+			Rds: &hcm.Rds{
+				ConfigSource:    &core.ConfigSource{},
+				RouteConfigName: route,
+			},
+		},
+		HttpFilters: []*hcm.HttpFilter{{
+			Name: wellknown.Router,
+		}},
+	}
+	pbst, err := ptypes.MarshalAny(manager)
+	if err != nil {
+		panic(err)
+	}
+
+	return &listener.Listener{
+		Name:    ListenerName,
+		Address: &core.Address{},
+		FilterChains: []*listener.FilterChain{{
+			Filters: []*listener.Filter{{
+				Name: wellknown.HTTPConnectionManager,
+				ConfigType: &listener.Filter_TypedConfig{
+					TypedConfig: pbst,
+				},
+			}},
+		}},
+	}
 }
--- pkg/api/v1/control-plane/cache/simple.go
+++ pkg/api/v1/control-plane/cache/simple.go
@@ -139,10 +139,18 @@ func (cache *snapshotCache) SetSnapshot(node string, snapshot Snapshot) error {
 				if cache.log != nil {
 					cache.log.Infof("respond open watch %d%v with new version %q", id, watch.Request.ResourceNames, version)
 				}
-				cache.respond(watch.Request, watch.Response, snapshot.GetResources(watch.Request.TypeUrl).Items, version)
 
-				// discard the watch
-				delete(info.watches, id)
+				resources := snapshot.GetResources(watch.Request.TypeUrl).Items
+				// Before sending a response, need to be able to differentiate between a resource in the snapshot that does not exist vs. should be empty
+				// nil resource - not set in the snapshot and should not be updated
+				// empty resource - intended behavior is for the resources to be cleared
+				if resources != nil {
+					// snapshot has been initialized and exists
+					cache.respond(watch.Request, watch.Response, resources, version)
+
+					// discard the watch
+					delete(info.watches, id)
+				}
 			}
 		}
 		info.mu.Unlock()
@@ -201,7 +209,7 @@ func (cache *snapshotCache) CreateWatch(request Request) (chan Response, func())
 
 	info, ok := cache.status[nodeID]
 	if !ok {
-		info = newStatusInfo(request.Node)
+		info = NewStatusInfo(request.Node)
 		cache.status[nodeID] = info
 	}
 
