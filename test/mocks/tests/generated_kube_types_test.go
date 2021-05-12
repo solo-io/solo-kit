e10a828e7cdd34a70eb90f35048607827b93be6d
Jahvon Dockery <jahvondockery@gmail.com>
2021-05-12 13:58:50 -0400

    spacing

--- test/mocks/tests/generated_kube_types_test.go
+++ test/mocks/tests/generated_kube_types_test.go
@@ -18,7 +18,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// go:generate bash ../v2alpha1/kube/hack/update-codegen.sh
+//go:generate bash ../v2alpha1/kube/hack/update-codegen.sh
 
 var _ = Describe("Generated Kube Code", func() {
 	var (
