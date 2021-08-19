639549f4e15b3b39fe8c2eeb847eacca7db84d22
Sam Heilbron <samheilbron@gmail.com>
2021-08-19 06:51:55 -0700

    include POD_NAMESPACE in test

--- pkg/api/v1/clients/kube/kube_suite_test.go
+++ pkg/api/v1/clients/kube/kube_suite_test.go
@@ -1,11 +1,28 @@
 package kube_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/solo-io/solo-kit/test/helpers"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+var namespace string
+
+var _ = BeforeSuite(func() {
+	namespace = helpers.RandString(5)
+
+	err := os.Setenv("POD_NAMESPACE", namespace)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	err := os.Unsetenv("POD_NAMESPACE")
+	Expect(err).NotTo(HaveOccurred())
+})
 
 func TestKube(t *testing.T) {
 	RegisterFailHandler(Fail)
