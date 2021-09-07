02044249d1492798f6e1a8d0dc85725674e65886
Sam Heilbron <samheilbron@gmail.com>
2021-09-07 12:09:17 -0400

    expose InputResource status unmarshal as method

--- pkg/api/v1/clients/kube/resource_client.go
+++ pkg/api/v1/clients/kube/resource_client.go
@@ -7,9 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
-
-	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
 
 	"github.com/solo-io/go-utils/stringutils"
@@ -446,54 +443,8 @@ func (rc *ResourceClient) convertCrdToResource(resourceCrd *v1.Resource) (resour
 		// Default unmarshalling
 
 		if withStatus, ok := resource.(resources.InputResource); ok {
-			// Always initialize status to empty, before it was empty by default, as it was a non-pointer value.
-			if err := withStatus.SetStatusForNamespace(&core.Status{}); err != nil {
+			if err := resources.UnmarshalInputResourceStatus(withStatus, resourceCrd.Status); err != nil {
 				return nil, err
-			}
-
-			updateStatusFunc := func(status *core.Status) error {
-				if status == nil {
-					return nil
-				}
-				typedStatus := core.Status{}
-				if err := protoutils.UnmarshalMapToProto(resourceCrd.Status, &typedStatus); err != nil {
-					return err
-				}
-				*status = typedStatus
-				return nil
-			}
-			updateNamespacedStatusesFunc := func(status *core.NamespacedStatuses) error {
-				if status == nil {
-					return nil
-				}
-				typedStatus := core.NamespacedStatuses{}
-				if err := protoutils.UnmarshalMapToProto(resourceCrd.Status, &typedStatus); err != nil {
-					return err
-				}
-				*status = typedStatus
-				return nil
-			}
-
-			// Unmarshal the status from the Resource
-			// To support Resources that have Statuses either of type core.Status or core.NamespacedStatuses
-			//	we perform this unmarshalling in a couple of steps:
-			//
-			// 1. Attempt to unmarshal the status as a core.NamespacedStatus. Resources will be persisted with this type
-			//	moving forward, so we attempt this unmarshalling first.
-			// 2. If we are successful, complete
-			// 3. If we are not successful, attempt to unmarshal the status as a core.Status.
-			// 4. If we are successful, update the Status for this namespace
-			// 5. If we are not successful, an error has occurred.
-			if namespacedStatusesErr := resources.UpdateNamespacedStatuses(withStatus, updateNamespacedStatusesFunc); namespacedStatusesErr != nil {
-				// If unmarshalling NamespacedStatuses failed, the resource likely has a Status instead.
-				statusErr := resources.UpdateStatusForNamespace(withStatus, updateStatusFunc)
-				if statusErr != nil {
-					// There's actually something wrong if either status can't be unmarshalled.
-					var multiErr *multierror.Error
-					multiErr = multierror.Append(multiErr, namespacedStatusesErr)
-					multiErr = multierror.Append(multiErr, statusErr)
-					return nil, multiErr
-				}
 			}
 		}
 		if resourceCrd.Spec != nil {
--- pkg/api/v1/resources/resource_interface.go
+++ pkg/api/v1/resources/resource_interface.go
@@ -5,6 +5,9 @@ import (
 	"reflect"
 	"sort"
 
+	"github.com/hashicorp/go-multierror"
+	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
+
 	v1 "github.com/solo-io/solo-kit/pkg/api/v1/clients/kube/crd/solo.io/v1"
 
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -473,6 +476,61 @@ func UpdateNamespacedStatuses(resource InputResource, updateFunc func(namespaced
 		return err
 	}
 	resource.SetNamespacedStatuses(namespacedStatuses)
+	return nil
+}
+
+func UnmarshalInputResourceStatus(inputResource InputResource, resourceStatus map[string]interface{}) error {
+	// Always initialize status to empty, before it was empty by default, as it was a non-pointer value.
+	if err := inputResource.SetStatusForNamespace(&core.Status{}); err != nil {
+		return err
+	}
+
+	updateStatusFunc := func(status *core.Status) error {
+		if status == nil {
+			return nil
+		}
+		typedStatus := core.Status{}
+		if err := protoutils.UnmarshalMapToProto(resourceStatus, &typedStatus); err != nil {
+			return err
+		}
+		*status = typedStatus
+		return nil
+	}
+
+	updateNamespacedStatusesFunc := func(status *core.NamespacedStatuses) error {
+		if status == nil {
+			return nil
+		}
+		typedStatus := core.NamespacedStatuses{}
+		if err := protoutils.UnmarshalMapToProto(resourceStatus, &typedStatus); err != nil {
+			return err
+		}
+		*status = typedStatus
+		return nil
+	}
+
+	// Unmarshal the status from the Resource
+	// To support Resources that have Statuses either of type core.Status or core.NamespacedStatuses
+	//	we perform this unmarshalling in a couple of steps:
+	//
+	// 1. Attempt to unmarshal the status as a core.NamespacedStatus. Resources will be persisted with this type
+	//	moving forward, so we attempt this unmarshalling first.
+	// 2. If we are successful, complete
+	// 3. If we are not successful, attempt to unmarshal the status as a core.Status.
+	// 4. If we are successful, update the Status for this namespace
+	// 5. If we are not successful, an error has occurred.
+	if namespacedStatusesErr := UpdateNamespacedStatuses(inputResource, updateNamespacedStatusesFunc); namespacedStatusesErr != nil {
+		// If unmarshalling NamespacedStatuses failed, the resource likely has a Status instead.
+		statusErr := UpdateStatusForNamespace(inputResource, updateStatusFunc)
+		if statusErr != nil {
+			// There's actually something wrong if either status can't be unmarshalled.
+			var multiErr *multierror.Error
+			multiErr = multierror.Append(multiErr, namespacedStatusesErr)
+			multiErr = multierror.Append(multiErr, statusErr)
+			return multiErr
+		}
+	}
+
 	return nil
 }
 
