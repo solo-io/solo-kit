1787729a3d8d45c49df2a4b4ec6cd7b7ac5d187f
Fabian Gonzalez <fabiangonz98@gmail.com>
2022-04-21 11:12:23 -0400

    Add default query options to avoid nil referencing + generalize Watch()

--- pkg/api/v1/clients/consul/resource_client_test.go
+++ pkg/api/v1/clients/consul/resource_client_test.go
@@ -3,6 +3,7 @@ package consul_test
 import (
 	"context"
 	"fmt"
+	v1 "github.com/solo-io/solo-kit/test/mocks/v1"
 	"time"
 
 	"github.com/hashicorp/consul/api"
@@ -11,16 +12,14 @@ import (
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	. "github.com/solo-io/solo-kit/pkg/api/v1/clients/consul"
 	"github.com/solo-io/solo-kit/test/helpers"
-	v1 "github.com/solo-io/solo-kit/test/mocks/v1"
 	"github.com/solo-io/solo-kit/test/tests/generic"
 )
 
 var _ = Describe("Base", func() {
 	var (
-		consul       *api.Client
-		client       *ResourceClient
-		queryOptions *api.QueryOptions
-		rootKey      string
+		consul  *api.Client
+		client  *ResourceClient
+		rootKey string
 	)
 	BeforeEach(func() {
 		rootKey = "my-root-key"
@@ -32,13 +31,26 @@ var _ = Describe("Base", func() {
 		Expect(err).NotTo(HaveOccurred())
 		consul = c
 
-		queryOptions = &api.QueryOptions{AllowStale: false, RequireConsistent: true}
-		client = NewResourceClient(consul, rootKey, queryOptions, &v1.MockResource{})
 	})
 	AfterEach(func() {
 		consul.KV().DeleteTree(rootKey, nil)
 	})
 	It("CRUDs resources", func() {
+		queryOptions := &api.QueryOptions{AllowStale: false, RequireConsistent: true}
+		client = NewResourceClient(consul, rootKey, queryOptions, &v1.MockResource{})
+
+		selector := map[string]string{
+			helpers.TestLabel: helpers.RandString(8),
+		}
+		generic.TestCrudClient("ns1", "ns2", client, clients.WatchOpts{
+			Selector:    selector,
+			Ctx:         context.TODO(),
+			RefreshRate: time.Minute,
+		})
+	})
+	It("CRUDs resources when no query options are passed", func() {
+		client = NewResourceClient(consul, rootKey, nil, &v1.MockResource{})
+
 		selector := map[string]string{
 			helpers.TestLabel: helpers.RandString(8),
 		}
