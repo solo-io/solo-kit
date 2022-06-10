ef8359f18665fb91a9c731e7c8e2e820c999da1f
Jake Cukjati <jakecukjati@gmail.com>
2022-06-10 08:06:49 -0500

    init commit?

--- pkg/api/external/kubernetes/configmap/configmap_suite_test.go
+++ pkg/api/external/kubernetes/configmap/configmap_suite_test.go
@@ -7,6 +7,11 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var _ = func() bool {
+	testing.Init()
+	return true
+}()
+
 func TestNamespace(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Configmap Suite")
