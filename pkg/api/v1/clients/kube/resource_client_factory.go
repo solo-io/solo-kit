f3597e0c5cae7be1b567a3e51cfa4bd026a3b9e1
Jake Cukjati <jakecukjati@gmail.com>
2022-09-13 14:13:36 -0500

    better mutex control

--- pkg/api/v1/clients/kube/resource_client_factory.go
+++ pkg/api/v1/clients/kube/resource_client_factory.go
@@ -99,6 +99,9 @@ func NewKubeSharedCacheForConfig(ctx context.Context, cluster string, restConfig
 // and, when started, creates a kubernetes controller that distributes notifications for changes to the watches that
 // have been added to the factory.
 // All direct operations on the ResourceClientSharedInformerFactory are synchronized.
+// Note it might be best to use this as a Singleton instead of a Structure
+// if a Singleton suggestion is proposed, then Register will no long be needed,
+// and we can just use Register New Namespace.  Also this would clear up some mutexes, that are being used.
 type ResourceClientSharedInformerFactory struct {
 	// Contains all the informers managed by this factory
 	registry *informerRegistry
@@ -128,8 +131,10 @@ type ResourceClientSharedInformerFactory struct {
 	// registerNamespaceLock is a map of string(namespace) -> sync.Once. It is used to register a new namespace.
 	registerNamespaceLock sync.Map
 
-	// Mutexes
-	lock                      sync.Mutex
+	// registryLock is used when adding or getting information from the registry
+	registryLock sync.Mutex
+	// startingLock is used when checking isRunning or to lock starting of the SharedInformer
+	startingLock              sync.Mutex
 	cacheUpdatedWatchersMutex sync.Mutex
 }
 
@@ -178,14 +183,19 @@ func NewSharedInformer(ctx context.Context, resyncPeriod time.Duration, objType 
 // NOTE: Currently we cannot share informers between resource clients, because the listWatch functions are configured
 // with the client's specific token. Hence, we must enforce a one-to-one relationship between informers and clients.
 func (f *ResourceClientSharedInformerFactory) Register(rc *ResourceClient) error {
+	// because we do not know that we have started yet or not, we need to make a lock
+	// once we know, we can proceed
+	f.startingLock.Lock()
 	ctx := f.ctx
 	if f.started {
+		f.startingLock.Unlock()
 		for _, ns := range rc.namespaceWhitelist {
 			if err := f.RegisterNewNamespace(ns, rc); err != nil {
 				return err
 			}
 		}
 	} else {
+		defer f.startingLock.Unlock()
 		if ctxWithTags, err := tag.New(ctx, tag.Insert(KeyKind, rc.resourceName)); err == nil {
 			ctx = ctxWithTags
 		}
@@ -196,6 +206,8 @@ func (f *ResourceClientSharedInformerFactory) Register(rc *ResourceClient) error
 		// to be able to change the Informer so that it does not watch on "", but only those namespaces
 		// that we want to watch.  This only happens if the client(user) is setting the namespaceLabelSelectors field in the API.
 		for _, ns := range rc.namespaceWhitelist {
+			// we want to make sure that we have registered this namespace
+			f.registerNamespaceLock.LoadOrStore(ns, &sync.Once{})
 			if _, err := f.addNewNamespaceToRegistry(ctx, ns, rc); err != nil {
 				return err
 			}
@@ -206,8 +218,8 @@ func (f *ResourceClientSharedInformerFactory) Register(rc *ResourceClient) error
 
 // addNewNamespaceToRegistry will create a watch for the resource client type and namespace
 func (f *ResourceClientSharedInformerFactory) addNewNamespaceToRegistry(ctx context.Context, ns string, rc *ResourceClient) (cache.SharedIndexInformer, error) {
-	f.lock.Lock()
-	defer f.lock.Unlock()
+	f.registryLock.Lock()
+	defer f.registryLock.Unlock()
 
 	nsCtx := ctx
 	resourceType := reflect.TypeOf(rc.crd.Version.Type)
@@ -253,9 +265,14 @@ var cacheSyncTimeout = func() time.Duration {
 // Starts all informers in the factory's registry (if they have not yet been started) and configures the factory to call
 // the updateCallback function whenever any of the resources associated with the informers changes.
 func (f *ResourceClientSharedInformerFactory) Start() {
+	// if starting and managing the locks becomes to much a problem, we can start the factory
+	// when the factory is instantiated. That way we will not have multiple processes trying to Start() this structure
+	// which should be a singleton.
 
 	// Guarantees that the factory will be started at most once
 	f.factoryStarter.Do(func() {
+		f.startingLock.Lock()
+		defer f.startingLock.Unlock()
 		ctx := f.ctx
 
 		// Collect all registered informers
@@ -297,8 +314,9 @@ func (f *ResourceClientSharedInformerFactory) Start() {
 // RegisterNewNamespace is used when the resource client is running. This will add a new namespace to the
 // kube controller so that events can be received.
 func (f *ResourceClientSharedInformerFactory) RegisterNewNamespace(namespace string, rc *ResourceClient) error {
-	// when does the start function get called?
-	// the once function below is adding a namespace once and only once
+	// because this is an exposed function, Register New Namespace could be called at any time
+	// in that event, we want to make sure that the cache has started, the reason is because
+	// we have to initiallize the default namespaces as well as this new namespace
 	f.Start()
 	// we should only register a namespace once and only once
 	once, _ := f.registerNamespaceLock.LoadOrStore(namespace, &sync.Once{})
@@ -321,8 +339,8 @@ func (f *ResourceClientSharedInformerFactory) RegisterNewNamespace(namespace str
 }
 
 func (f *ResourceClientSharedInformerFactory) GetLister(namespace string, obj runtime.Object) (ResourceLister, error) {
-	f.lock.Lock()
-	defer f.lock.Unlock()
+	f.registryLock.Lock()
+	defer f.registryLock.Unlock()
 
 	// If the factory (and hence the informers) have not been started, the list operations will be meaningless.
 	// Will not happen in our current use of this, but since this method is public it's worth having this check.
