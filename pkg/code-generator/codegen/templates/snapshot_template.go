951641823f2d9ac3a9799d5e345e673b3de8db3c
Jacob Cukjati <jakecukjati@gmail.com>
2022-10-21 10:11:55 -0500

    Update pkg/code-generator/codegen/templates/snapshot_template.go
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>

--- pkg/code-generator/codegen/templates/snapshot_template.go
+++ pkg/code-generator/codegen/templates/snapshot_template.go
@@ -110,7 +110,7 @@ func (s *{{ .GoName }}Snapshot) RemoveFromResourceList(resource resources.Resour
 		return nil	
 {{- end }}
 	default:
-		return eris.Errorf("did not remove the reousource because its type does not exist [%T]", resource)
+		return eris.Errorf("did not remove the resource because its type does not exist [%T]", resource)
 	}
 }
 
