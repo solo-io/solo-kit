d5ac87d18a66d4fb7ea0973c681315e007b04142
Nathan Fudenberg <nathan.fudenberg@solo.io>
2022-12-14 11:58:00 -0500

    Revert "Feat/k8s 125"

--- test/mocks/v2alpha1/kube/client/clientset/versioned/typed/testing.solo.io/v2alpha1/testing.solo.io_client.go
+++ test/mocks/v2alpha1/kube/client/clientset/versioned/typed/testing.solo.io/v2alpha1/testing.solo.io_client.go
@@ -19,8 +19,6 @@ limitations under the License.
 package v2alpha1
 
 import (
-	"net/http"
-
 	v2alpha1 "github.com/solo-io/solo-kit/test/mocks/v2alpha1/kube/apis/testing.solo.io/v2alpha1"
 	"github.com/solo-io/solo-kit/test/mocks/v2alpha1/kube/client/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
@@ -46,28 +44,12 @@ func (c *TestingV2alpha1Client) MockResources(namespace string) MockResourceInte
 }
 
 // NewForConfig creates a new TestingV2alpha1Client for the given config.
-// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
-// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*TestingV2alpha1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	httpClient, err := rest.HTTPClientFor(&config)
-	if err != nil {
-		return nil, err
-	}
-	return NewForConfigAndClient(&config, httpClient)
-}
-
-// NewForConfigAndClient creates a new TestingV2alpha1Client for the given config and http client.
-// Note the http client provided takes precedence over the configured transport values.
-func NewForConfigAndClient(c *rest.Config, h *http.Client) (*TestingV2alpha1Client, error) {
-	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
-	client, err := rest.RESTClientForConfigAndClient(&config, h)
+	client, err := rest.RESTClientFor(&config)
 	if err != nil {
 		return nil, err
 	}
