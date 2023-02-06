a7b80fd00d3c28475c4a276b8b399476ee6a8d54
Sam Heilbron <SamHeilbron@gmail.com>
2023-02-06 17:10:17 -0500

    Update pkg/code-generator/codegen/templates/event_loop_test_template.go
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>

--- pkg/code-generator/codegen/templates/event_loop_test_template.go
+++ pkg/code-generator/codegen/templates/event_loop_test_template.go
@@ -5,7 +5,7 @@ import (
 )
 
 var ResourceGroupEventLoopTestTemplate = template.Must(template.New("resource_group_event_loop_test").Funcs(Funcs).Parse(`
-// go:build solokit
+//go:build solokit
 
 package {{ .Project.ProjectConfig.Version }}
 
