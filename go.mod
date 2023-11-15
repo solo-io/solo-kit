abbd12e6999b0a1f0ff6309008bf9b6c3a123eb4
sheidkamp <seth.heidkamp@gmail.com>
2023-11-15 15:44:22 -0500

    initial changes

--- go.mod
+++ go.mod
@@ -52,6 +52,16 @@ require (
 )
 
 require (
+	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.6 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-uuid v1.0.2-0.20191001231223-f32f5fe8d6a8 // indirect
+	github.com/lyft/protoc-gen-star v0.6.0 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+)
+
+require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.4.2 // indirect
 	github.com/armon/go-metrics v0.3.0 // indirect
@@ -80,8 +90,8 @@ require (
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-hclog v0.9.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
-	github.com/hashicorp/go-retryablehttp v0.6.2 // indirect
-	github.com/hashicorp/go-rootcerts v1.0.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.6 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -107,7 +117,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
@@ -127,7 +136,6 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230526161137-0005af68ea54 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/square/go-jose.v2 v2.3.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/gengo v0.0.0-20220902162205-c0856e24416d // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
--- pkg/api/v1/clients/vault/resource_client.go
+++ pkg/api/v1/clients/vault/resource_client.go
@@ -1,6 +1,9 @@
 package vault
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"reflect"
 	"strconv"
 	"strings"
@@ -9,12 +12,14 @@ import (
 	"github.com/solo-io/solo-kit/pkg/api/shared"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 
-	"github.com/hashicorp/vault/api"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
 	"github.com/solo-io/solo-kit/pkg/errors"
 	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
 	"k8s.io/apimachinery/pkg/labels"
+
+	vault "github.com/hashicorp/vault/api"
+	auth "github.com/hashicorp/vault/api/auth/userpass"
 )
 
 const (
@@ -23,7 +28,7 @@ const (
 	checkAndSetKey = "cas"
 )
 
-func (rc *ResourceClient) fromVaultSecret(secret *api.Secret) (resources.Resource, bool, error) {
+func (rc *ResourceClient) fromVaultSecret(secret *vault.Secret) (resources.Resource, bool, error) {
 	if secret.Data == nil {
 		return nil, false, errors.Errorf("secret data cannot be nil")
 	}
@@ -69,7 +74,7 @@ func (rc *ResourceClient) toVaultSecret(resource resources.Resource) (map[string
 }
 
 type ResourceClient struct {
-	vault *api.Client
+	vault *vault.Client
 
 	// Vault's path where resources are located.
 	root string
@@ -80,7 +85,7 @@ type ResourceClient struct {
 	resourceType resources.VersionedResource
 }
 
-func NewResourceClient(client *api.Client, pathPrefix string, rootKey string, resourceType resources.VersionedResource) *ResourceClient {
+func NewResourceClient(client *vault.Client, pathPrefix string, rootKey string, resourceType resources.VersionedResource) *ResourceClient {
 	if pathPrefix == "" {
 		pathPrefix = "secret"
 	}
@@ -252,6 +257,7 @@ func (rc *ResourceClient) listSingleNamespace(namespace string, opts clients.Lis
 func (rc *ResourceClient) listKeys(directory string) ([]string, error) {
 	keyList, err := rc.vault.Logical().List(directory)
 	if err != nil {
+		renewToken(rc.vault)
 		return nil, errors.Wrapf(err, "listing directory %v", directory)
 	}
 	if keyList == nil {
@@ -342,4 +348,108 @@ func (rc *ResourceClient) resourceMetadataKey(namespace, name string) string {
 	return strings.Join([]string{
 		rc.resourceDirectory(namespace, directoryTypeMetadata),
 		name}, "/")
+}
+
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+
+// 	vault "github.com/hashicorp/vault/api"
+// 	auth "github.com/hashicorp/vault/api/auth/userpass"
+// )
+
+// Once you've set the token for your Vault client, you will need to
+// periodically renew its lease.
+//
+// A function like this should be run as a goroutine to avoid blocking.
+//
+// Production applications may also wish to be more tolerant of failures and
+// retry rather than exiting.
+//
+// Additionally, enterprise Vault users should be aware that due to eventual
+// consistency, the API may return unexpected errors when running Vault with
+// performance standbys or performance replication, despite the client having
+// a freshly renewed token. See https://www.vaultproject.io/docs/enterprise/consistency#vault-1-7-mitigations
+// for several ways to mitigate this which are outside the scope of this code sample.
+func renewToken(client *vault.Client) {
+	for {
+		vaultLoginResp, err := login(client)
+		if err != nil {
+			log.Fatalf("unable to authenticate to Vault: %v", err)
+		}
+		tokenErr := manageTokenLifecycle(client, vaultLoginResp)
+		if tokenErr != nil {
+			log.Fatalf("unable to start managing token lifecycle: %v", tokenErr)
+		}
+	}
+}
+
+// Starts token lifecycle management. Returns only fatal errors as errors,
+// otherwise returns nil so we can attempt login again.
+func manageTokenLifecycle(client *vault.Client, token *vault.Secret) error {
+	renew := token.Auth.Renewable // You may notice a different top-level field called Renewable. That one is used for dynamic secrets renewal, not token renewal.
+	if !renew {
+		log.Printf("Token is not configured to be renewable. Re-attempting login.")
+		return nil
+	}
+
+	watcher, err := client.NewLifetimeWatcher(&vault.LifetimeWatcherInput{
+		Secret:    token,
+		Increment: 3600, // Learn more about this optional value in https://www.vaultproject.io/docs/concepts/lease#lease-durations-and-renewal
+	})
+	if err != nil {
+		return fmt.Errorf("unable to initialize new lifetime watcher for renewing auth token: %w", err)
+	}
+
+	go watcher.Start()
+	defer watcher.Stop()
+
+	for {
+		select {
+		// `DoneCh` will return if renewal fails, or if the remaining lease
+		// duration is under a built-in threshold and either renewing is not
+		// extending it or renewing is disabled. In any case, the caller
+		// needs to attempt to log in again.
+		case err := <-watcher.DoneCh():
+			if err != nil {
+				log.Printf("Failed to renew token: %v. Re-attempting login.", err)
+				return nil
+			}
+			// This occurs once the token has reached max TTL.
+			log.Printf("Token can no longer be renewed. Re-attempting login.")
+			return nil
+
+		// Successfully completed renewal
+		case renewal := <-watcher.RenewCh():
+			log.Printf("Successfully renewed: %#v", renewal)
+		}
+	}
+}
+
+func login(client *vault.Client) (*vault.Secret, error) {
+	// WARNING: A plaintext password like this is obviously insecure.
+	// See the files in the auth-methods directory for full examples of how to securely
+	// log in to Vault using various auth methods. This function is just
+	// demonstrating the basic idea that a *vault.Secret is returned by
+	// the login call.
+	userpassAuth, err := auth.NewUserpassAuth("my-user", &auth.Password{FromString: "my-password"})
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize userpass auth method: %w", err)
+	}
+
+	authInfo, err := client.Auth().Login(context.Background(), userpassAuth)
+	if err != nil {
+		return nil, fmt.Errorf("unable to login to userpass auth method: %w", err)
+	}
+	if authInfo == nil {
+		return nil, fmt.Errorf("no auth info was returned after login")
+	}
+
+	return authInfo, nil
 }
